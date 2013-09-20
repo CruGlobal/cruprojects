b08fcc56b11850b0b74b74f8c343c843a8eab020
Josh Starcher <josh.starcher@gmail.com>
2013-09-20 15:46:15 -0400

    Try to save github token on every request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
   before_filter :authorize_from_github
 
   def authorize_from_github
-    unless session[:token]
+    if session[:token]
+      # make sure the token is stored
+      if team_member = TeamMember.find_by(github_login: session[:user])
+        team_member.update_attributes(access_token: session[:token])
+      end
+    else
       session[:return_to] = request.path
       redirect_to '/auth/github'
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,6 @@ class SessionsController < ApplicationController
   def create
     session[:user] = auth_hash.info.nickname
     session[:token]= auth_hash.credentials.token
-    if team_member = TeamMember.find_by(github_login: session[:user])
-      team_member.update_attributes(access_token: session[:token])
-    end
 
     redirect_to(session[:return_to] || '/')
   end
