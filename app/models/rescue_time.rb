e346b968eb22d670f97f449687a03e35a05a9c56
Josh Starcher <josh.starcher@gmail.com>
2013-10-26 18:31:11 -0400

    Fix viewing of prior rescue time days

--- app/models/rescue_time.rb
+++ app/models/rescue_time.rb
@@ -5,7 +5,7 @@ class RescueTime
   end
 
   def data(start_date, end_date)
-    json = JSON.parse(RestClient.get("https://www.rescuetime.com/anapi/data?format=json&key=#{@team_member.rescue_time_token}&perspective=interval&resolution_time=day&restrict_kind=category&restrict_begin=#{start_date.to_s(:db)}"))
+    json = JSON.parse(RestClient.get("https://www.rescuetime.com/anapi/data?format=json&key=#{@team_member.rescue_time_token}&perspective=interval&resolution_time=day&restrict_kind=category&restrict_begin=#{start_date.to_s(:db)}&restrict_end=#{end_date.to_s(:db)}"))
     json['rows'] || []
   end
 
