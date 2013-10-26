54539ef32f89cfaaa0f6a4a58a75e29c5efad904
Josh Starcher <josh.starcher@gmail.com>
2013-10-26 19:14:28 -0400

    Remove test stubs

--- test/controllers/team_members_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TeamMembersControllerTest < ActionController::TestCase
-  test "should get rescue" do
-    get :rescue
-    assert_response :success
-  end
-
-end
--- test/models/rescue_time_category_day_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RescueTimeCategoryDayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
