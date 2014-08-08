5f01ac4e27c5afe009676cc56ff7761667fd9d53
Josh Starcher <josh.starcher@gmail.com>
2014-08-08 16:23:27 -0400

    Tell omniauth to ignore cert errors in dev mode

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Rails.configuration.github_key, Rails.configuration.github_secret, scope: 'user,user:email,repo'
 end
